63d723928c41142b6a35c9926e331c7849e71648
gfalbery <gfalbery@gmail.com>
2022-11-17 12:11:41 +0000

    Adding linear models

--- Greg Code/02b_BISoN Annual Summarising.R
+++ Greg Code/02b_BISoN Annual Summarising.R
@@ -1,7 +1,9 @@
 
 # 02a_BISoN Annual Summarising ####
 
-library(tidyverse)
+library(tidyverse); library(ggregplot); library(ggforce); library(cowplot)
+
+theme_set(theme_cowplot())
 
 IndivListList <- 
   "Greg Data/Outputs/BISoN" %>% 
@@ -21,7 +23,18 @@ Maxes <-
     
   }) %>% bind_rows()
 
-(LongMaxes <- Maxes %>% reshape2::melt())
+Means <- 
+  
+  IndivListList %>% 
+  
+  map(function(a){
+    
+    map(a, ~mean(.x$Time)) %>% unlist
+    
+  }) %>% bind_rows()
+
+(LongMaxes <- Maxes %>% reshape2::melt() %>% rename(Max = 2) %>% 
+    bind_cols(Means %>% reshape2::melt() %>% rename(Mean = 2) %>% dplyr::select(-1)))
 
 LongMaxes %<>% 
   separate(variable, sep = "_", into = c("PI", "Rep"))
@@ -31,17 +44,49 @@ LongMaxes %<>%
   mutate(Population = substr(Rep, 1, 1), 
          Year = substr(Rep, 2, 5) %>% as.numeric)
 
-LongMaxes %>% 
-  ggplot(aes(value, colour = Rep)) + 
-  geom_density() +
-  facet_wrap(~Year)
+LongMaxes %<>% mutate(PostMaria = as.factor(as.numeric(Year >= 2018)))
 
+library(patchwork)
 
 LongMaxes %>% 
-  ggplot(aes(value, colour = Rep)) + 
-  geom_density() +
-  facet_wrap(~Year)
+  ggplot(aes(as.factor(Year), Max, colour = PostMaria)) +
+  geom_boxplot() +
+  geom_sina(alpha = 0.1) +
+  facet_grid(PI~Population) + 
+  ggtitle("Max") +
+  scale_colour_manual(values = c(AlberColours[[1]], AlberColours[[2]])) +
+
+LongMaxes %>% 
+  ggplot(aes(as.factor(Year), Mean, colour = PostMaria)) +
+  geom_boxplot() +
+  geom_sina(alpha = 0.1) +
+  facet_grid(PI~Population) + 
+  ggtitle("Mean") +
+  scale_colour_manual(values = c(AlberColours[[1]], AlberColours[[2]])) +
+  
+  plot_layout(guides = "collect")
+
+ggsave("Figures/Pre_Post_BISoN.jpeg", units = "mm", width = 250, height = 150)
 
 
+# Running an LM ####
 
+TestDF <- IndivListList %>% 
+  map(~bind_rows(.x, .id = "Sim")) %>% 
+  bind_rows(.id = "Rep")
 
+TestDF %<>% 
+  # separate(Rep, sep = "_", into = c("PI", "Rep"))
+  mutate(PI = substr(Rep, 1, 3) %>% as.numeric) %>% 
+  mutate(Rep = str_split(Rep, "_") %>% map_chr(last))
+
+TestDF %<>% 
+  mutate_at("Rep", ~str_replace(.x, "KK", "K")) %>% 
+  mutate(Population = substr(Rep, 1, 1), 
+         Year = substr(Rep, 2, 5) %>% as.numeric)
+
+TestDF %<>% mutate(PostMaria = as.factor(as.numeric(Year >= 2018)))
+
+LM1 <- lm(Time ~ as.factor(PI) + PostMaria + Rep, data = TestDF)
+
+LM1 %>% summary
